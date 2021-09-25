b52444884953c4b246515c80ae282f5417c295ef
Mateus_Nobre <mateusnobresilva@gmail.com>
2021-09-25 12:33:19 -0300

    FEATURE Lombok and interface

---
+++ src/main/java/com/mateusnobre/bookstoragemanager/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.mateusnobre.bookstoragemanager.repository;
+
+import com.mateusnobre.bookstoragemanager.enity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository  extends JpaRepository <Book, Long> {
+}
